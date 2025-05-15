29e19659b4845516e9b92f8fdd5c8e6c0934c9fc
Uroš Preložnik <uros00@gmail.com>
2025-05-15 22:47:44 +0200

    title

--- doc/conf.py
+++ doc/conf.py
@@ -96,6 +96,8 @@ html_show_sourcelink = False
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_title = 'GJI Elaborat'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
